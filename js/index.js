40aa9ac33c0ab15ae52bb8c265bc97c31b43363b
Julia Pickel <juliapickel@gmx.de>
2023-10-10 17:18:45 +0200

    changes according to async function

--- js/index.js
+++ js/index.js
@@ -2,26 +2,26 @@ console.clear();
 
 const url = "https://swapi.dev/api/people";
 
-function fetchData() {
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data); // to log the entire data
-      // to count the number of characters / JS objects of the fetched data
-      const characterCount = data.results;
-      console.log("Number of characters: ", characterCount);
-      // all names of characters:
-      console.log(
-        data.results.forEach((character) => {
-          console.log(character.name);
-        })
-      );
-      //Bonus: eye color of R2-D2:
-      const r2d2 = data.results.find((character) => character.name === "R2-D2");
-      if (r2d2) {
-        console.log(r2d2.eye_color);
-      }
-    });
+async function fetchData() {
+  const response = await fetch(url);
+  const data = await response.json();
+
+  // to log the entire data:
+  await console.log(data);
+  // to count the number of characters / JS objects of the fetched data
+  const characterCount = data.results;
+  await console.log("Number of characters: ", characterCount);
+  // all names of characters:
+  await console.log(
+    data.results.forEach((character) => {
+      console.log(character.name);
+    })
+  );
+  //Bonus: eye color of R2-D2:
+  await console.log(
+    "Eye color of R2D2: ",
+    data.results.find((character) => character.name === "R2-D2").eye_color
+  );
 }
 
-fetchData();
+await fetchData();
